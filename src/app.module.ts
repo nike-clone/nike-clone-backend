742454c9d1d8b8c4d0facd814ab348a9deab2930
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-07-13 22:01:56 +0900

    feat: /api/admin 을 prefix로 하는 category, products router 생성
    - GET /api/admin/category 는 임시로 dummy category object를 반환함

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,19 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+// import { AdminModule } from './admin/admin.module';
+import { ProductsModule } from './products/products.module';
+import { ApiModule } from './api/api.module';
+import { CategoryModule } from './category/category.module';
+import { AdminModule } from './admin/admin.module';
+import { ApiController } from './api/api.controller';
+import { RouterModule } from '@nestjs/core';
+// import { AdminModule } from './admin/admin.module';
+
 @Module({
-  imports: [],
+  // imports: [AdminModule],
   controllers: [AppController],
   providers: [AppService],
+  imports: [ApiModule, ProductsModule],
 })
 export class AppModule {}
