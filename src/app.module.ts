177eef7e6217549d45cec0d991d41b075bbe3980
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-07-14 21:06:20 +0900

    feat: Production database 연결

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,8 +6,9 @@ import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 import { ApiModule } from './api/api.module';
 import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { AuroraDataApiConnectionOptions } from 'typeorm/driver/aurora-data-api/AuroraDataApiConnectionOptions';
+import OrmConfig from './ormConfig';
 
 @Module({
   controllers: [AppController],
@@ -17,16 +18,18 @@ import { AuroraDataApiConnectionOptions } from 'typeorm/driver/aurora-data-api/A
       envFilePath: [`${__dirname}/config/env/.${process.env.NODE_ENV}.env`],
       isGlobal: true,
     }),
-    TypeOrmModule.forRoot({
-      type: process.env.DATABASE_TYPE,
-      host: process.env.DATABASE_HOST,
-      port: +process.env.DATABASE_PORT,
-      username: process.env.DATABASE_USERNAME,
-      password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE_NAME,
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true,
-    } as AuroraDataApiConnectionOptions),
+    TypeOrmModule.forRoot(OrmConfig as TypeOrmModuleOptions),
+    // TypeOrmModule.forRoot({
+    //   type: process.env.DATABASE_TYPE,
+    //   host: process.env.DATABASE_HOST,
+    //   port: +process.env.DATABASE_PORT,
+    //   username: process.env.DATABASE_USERNAME,
+    //   password: process.env.DATABASE_PASSWORD,
+    //   database: process.env.DATABASE_NAME,
+
+    //   entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    //   synchronize: true,
+    // } as AuroraDataApiConnectionOptions),
     ApiModule,
     ProductsModule,
   ],
---
+++ src/ormConfig.ts
@@ -0,0 +1,39 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
+
+const envFilePath = `${__dirname}/config/env/.${process.env.NODE_ENV}.env`;
+dotenv.config({ path: envFilePath });
+
+export class OrmConfig {
+  getConfig() {
+    let ormConfig: TypeOrmModuleOptions;
+    switch (process.env.NODE_ENV) {
+      case 'development': {
+        ormConfig = {
+          type: process.env.DATABASE_TYPE,
+          host: process.env.DATABASE_HOST,
+          port: +process.env.DATABASE_PORT,
+          username: process.env.DATABASE_USERNAME,
+          password: process.env.DATABASE_PASSWORD,
+          database: process.env.DATABASE_NAME,
+          enities: [__dirname + '/**/*.entity{.ts,.js}'],
+          synchronize: true,
+        } as TypeOrmModuleOptions;
+        break;
+      }
+      case 'production': {
+        ormConfig = {
+          host: process.env.DATABASE_HOST,
+          user: process.env.DATABASE_USER,
+          password: process.env.DATABASE_PASSWORD,
+          database: process.env.DATABASE_NAME,
+        } as TypeOrmModuleOptions;
+        break;
+      }
+    }
+
+    return ormConfig;
+  }
+}
+
+export default new OrmConfig().getConfig();
