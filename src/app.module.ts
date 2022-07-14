9a51ac28859ec0042ecca34893435eaf02070423
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-07-14 19:30:49 +0900

    feat: development database 연결

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,30 @@ import { AppService } from './app.service';
 // import { AdminModule } from './admin/admin.module';
 import { ProductsModule } from './products/products.module';
 import { ApiModule } from './api/api.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuroraDataApiConnectionOptions } from 'typeorm/driver/aurora-data-api/AuroraDataApiConnectionOptions';
 
 @Module({
   controllers: [AppController],
   providers: [AppService],
-  imports: [ApiModule, ProductsModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: [`${__dirname}/config/env/.${process.env.NODE_ENV}.env`],
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: process.env.DATABASE_TYPE,
+      host: process.env.DATABASE_HOST,
+      port: +process.env.DATABASE_PORT,
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    } as AuroraDataApiConnectionOptions),
+    ApiModule,
+    ProductsModule,
+  ],
 })
 export class AppModule {}
