a2a41ea0d5177f44adf45c019699733c5d9dc3b2
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-07-15 00:39:12 +0900

    feat: Category CRUD 구현
    Future works
    - Admin Guard 필요
    - GET Category 는 Guard 범위에서 제외

--- src/category/entities/category.entity.ts
+++ src/category/entities/category.entity.ts
@@ -1,8 +1,8 @@
-import { Column, Entity, PrimaryColumn } from 'typeorm';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity('Category')
 export class Category {
-  @PrimaryColumn()
+  @PrimaryGeneratedColumn()
   id: string;
 
   @Column()
