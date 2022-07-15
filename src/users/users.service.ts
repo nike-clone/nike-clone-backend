39ad2370b4e9a11ec6bd8e9f66e5aee3c1802fc3
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-07-15 17:01:10 +0900

    fix: usersRepository를 UsersService에 Inject
    - 수정 전 UsersService에 잘못 inject

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,9 +1,16 @@
 import { Injectable, NotAcceptableException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
+  constructor(
+    @InjectRepository(User) private usersRepository: Repository<User>,
+  ) {}
+
   async createUser(
     name: string,
     email: string,
@@ -34,7 +41,6 @@ export class UsersService {
   }
 
   private async checkUserExists(email: string) {
-    // todo DB
     return false;
   }
   private async checkPasswordsIdentical(password, passwordCheck) {
