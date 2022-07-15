eea4fb0c5fc5312bc66fc9c865728ce8d762ccac
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-07-15 16:59:33 +0900

    feat: Users Entity 생성
    - generate TypeORM database table

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,15 +1,29 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 import { Gender } from '../types/gender-type';
 
+@Entity('User')
 export class User {
+  @PrimaryGeneratedColumn()
   id: number;
 
+  @Column({ length: 60 })
+  email: string;
+
+  @Column({ length: 30 })
   password: string;
 
+  @Column({ length: 30 })
   name: string;
 
-  phoen: string;
+  @Column({ length: 20 })
+  phone: string;
 
+  @Column()
   birthOfDate: Date;
 
+  @Column()
   gender: Gender;
+
+  @Column({ default: false })
+  isAdmin: boolean;
 }
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { InjectRepository, TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
-  providers: [UsersService]
+  providers: [UsersService],
 })
-export class UsersModule {}
+export class UsersModule {
+  constructor(
+    @InjectRepository(User) private usersRepository: Repository<User>,
+  ) {}
+}
