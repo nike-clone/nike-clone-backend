29e50ac65e813bfc23fff6c64984fd2407c3e1e8
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-07-15 17:16:53 +0900

    feat: create user and save
    - request body의 user creation 정보로 user entity 생성 후 저장

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,9 +1,13 @@
 import { Injectable, NotAcceptableException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { stringify } from 'querystring';
 import { Repository } from 'typeorm';
+import { ulid } from 'ulid';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './entities/user.entity';
+import { Gender } from './types/gender-type';
+import * as uuid from 'uuid';
 
 @Injectable()
 export class UsersService {
@@ -12,16 +16,36 @@ export class UsersService {
   ) {}
 
   async createUser(
-    name: string,
     email: string,
     password: string,
     passwordCheck: string,
+    name: string,
+    phone: string,
+    birthOfDate: Date,
+    gender: Gender,
   ) {
     await this.checkUserExists(email);
 
     await this.checkPasswordsIdentical(password, passwordCheck);
 
-    return 'This action adds a new user';
+    const signupVerifyToken = uuid.v1();
+
+    const user = new User();
+    user.id = ulid();
+    user.name = name;
+    user.email = email;
+    user.password = password;
+    user.phone = phone;
+    user.birthOfDate = birthOfDate;
+    user.gender = gender;
+    user.signupVerifyToken = signupVerifyToken;
+
+    await this.usersRepository.save(user);
+
+    return {
+      message: 'User successfully created.',
+      data: user,
+    };
   }
 
   findAll() {
@@ -41,8 +65,12 @@ export class UsersService {
   }
 
   private async checkUserExists(email: string) {
-    return false;
+    const user = await this.usersRepository.findOne({ email });
+    if (user) {
+      throw new NotAcceptableException('User already exists.');
+    }
   }
+
   private async checkPasswordsIdentical(password, passwordCheck) {
     if (password !== passwordCheck) {
       throw new NotAcceptableException(
