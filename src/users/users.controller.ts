49713ee06225b84eacfffa21f8a4f3e098c698a4
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-07-16 01:46:27 +0900

    feat: AuthGaurd 구현
    - JWT 토큰 유효성 검사 로직
    - getUserInfo() 에 AuthGuard 적용

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,6 +8,7 @@ import {
   Delete,
   Query,
   Headers,
+  UseGuards,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
@@ -16,6 +17,7 @@ import { VerifyEmailDto } from './dto/verify-email.dto';
 import { UserLoginDto } from './dto/user-login.dto';
 import { UserInfo } from './UserInfo';
 import { AuthService } from 'src/auth/auth.service';
+import { AuthGuard } from 'src/Guards/auth.guard';
 
 @Controller('users')
 export class UsersController {
@@ -52,15 +54,12 @@ export class UsersController {
     return await this.usersService.login(email, password);
   }
 
+  @UseGuards(AuthGuard)
   @Get('/:id')
   async getUserInfo(
     @Headers() headers: any,
     @Param('id') userId: string,
   ): Promise<UserInfo> {
-    const jwtString = headers.authorization.split('Bearer ')[1];
-
-    this.authService.verify(jwtString);
-
     return this.usersService.getUserInfo(userId);
   }
 
