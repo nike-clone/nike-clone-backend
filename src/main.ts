aa391dde62349a094ee3e239b52d0b11ecfde20a
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-07-19 18:36:03 +0900

    feat: serve-static 추가

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,7 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
   await app.listen(process.env.PORT || 3000);
 
   console.log(`server running on port ${process.env.PORT || 3000}`);
