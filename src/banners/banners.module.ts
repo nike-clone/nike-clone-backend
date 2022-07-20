3da9855d7d6bda05d242a2d21f9810e647081e93
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-07-21 01:03:07 +0900

    feat: create banner 에 AdminGuard 적용
    - POST banner

--- src/banners/banners.module.ts
+++ src/banners/banners.module.ts
@@ -3,10 +3,11 @@ import { BannersService } from './banners.service';
 import { BannersController } from './banners.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Banner } from './entities/banner.entity';
+import { AuthService } from 'src/auth/auth.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Banner])],
   controllers: [BannersController],
-  providers: [BannersService],
+  providers: [BannersService, AuthService],
 })
 export class BannersModule {}
