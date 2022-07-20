ad60c901aa719e00338ffe3b39e46c41332e1a6d
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-07-21 00:41:48 +0900

    feat: Banner entity 정의

--- src/banners/entities/banner.entity.ts
+++ src/banners/entities/banner.entity.ts
@@ -1,1 +1,16 @@
-export class Banner {}
+import { Column, PrimaryGeneratedColumn } from 'typeorm';
+import { BannerType } from '../types/banner.type';
+
+export class Banner {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  imagePath: string;
+
+  @Column({ length: 65535 })
+  content: string;
+
+  @Column()
+  type: BannerType;
+}
