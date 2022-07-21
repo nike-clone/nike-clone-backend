86af9c51175548e2cbaa65cf43248a977edba982
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-07-21 18:17:30 +0900

    feat: Update banner 기능 구현

--- src/banners/dto/update-banner.dto.ts
+++ src/banners/dto/update-banner.dto.ts
@@ -1,4 +1,21 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { IsEnum, IsOptional, IsString } from 'class-validator';
 import { CreateBannerDto } from './create-banner.dto';
+enum BannerType {
+  Main = 'Main',
+  Promotion = 'Promotion',
+}
 
-export class UpdateBannerDto extends PartialType(CreateBannerDto) {}
+export class UpdateBannerDto extends PartialType(CreateBannerDto) {
+  @IsOptional()
+  @IsString()
+  iamge_path?: string;
+
+  @IsOptional()
+  @IsString()
+  content?: string;
+
+  @IsOptional()
+  @IsEnum(BannerType)
+  type?: BannerType;
+}
