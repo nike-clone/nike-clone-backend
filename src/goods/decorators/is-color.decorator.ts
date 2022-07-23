ec188e4ef346364da23983a5f77cd06469bcba50
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-07-23 16:35:11 +0900

    feat: IsColor decorator 구현
    - unacceptable color value filtering

---
+++ src/goods/decorators/is-color.decorator.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import {
+  registerDecorator,
+  ValidationArguments,
+  ValidationOptions,
+  ValidatorConstraint,
+  ValidatorConstraintInterface,
+} from 'class-validator';
+import { Repository } from 'typeorm';
+import { Color } from '../entities/colors.entity';
+
+@ValidatorConstraint({ name: 'IsColor', async: true })
+@Injectable()
+export class IsColorConstraint implements ValidatorConstraintInterface {
+  constructor(
+    @InjectRepository(Color) private colorRepository: Repository<Color>,
+  ) {}
+
+  async validate(color: any, args: ValidationArguments) {
+    const selectedColor = await this.colorRepository.findOne(color);
+    console.log(selectedColor);
+    if (!selectedColor) {
+      return false;
+    }
+
+    return true;
+  }
+
+  defaultMessage(args: ValidationArguments) {
+    return 'Unacceptable color value.';
+  }
+}
+
+export function IsColor(validationOptions?: ValidationOptions) {
+  return function (object: any, propertyName: string) {
+    registerDecorator({
+      name: 'IsColor',
+      target: object.constructor,
+      propertyName: propertyName,
+      constraints: [],
+      options: validationOptions,
+      validator: IsColorConstraint,
+    });
+  };
+}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { useContainer } from 'class-validator';
 import { AppModule } from './app.module';
+import { GoodsModule } from './goods/goods.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,7 +12,7 @@ async function bootstrap() {
     }),
   );
   app.enableCors();
-
+  useContainer(app.select(AppModule), { fallbackOnErrors: true });
   await app.listen(process.env.PORT || 3000);
 
   console.log(`server running on port ${process.env.PORT || 3000}`);
