a187bf272ef0a02583fa03b71af0820a0c4bad28
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-08-04 03:23:50 +0900

    typeorm 설정에 CartItem entity 추가

--- src/cart-items/cart-items.module.ts
+++ src/cart-items/cart-items.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { CartItemsService } from './cart-items.service';
 import { CartItemsController } from './cart-items.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CartItems } from './entities/cart-item.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([CartItems])],
   controllers: [CartItemsController],
-  providers: [CartItemsService]
+  providers: [CartItemsService],
 })
 export class CartItemsModule {}
